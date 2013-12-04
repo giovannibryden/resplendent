c92f0ceead19843592d172f7b3d414669ff52a5e
Giovanni Bryden <giovanni.bryden@moovweb.com>
2013-12-04 14:45:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resplendent::Application.config.secret_key_base = '240a2777b64177b2c63a05976c70497e474c8175403e851496b6b68cf12135968ff061a1b052e3e5ce7e56691f95818dd978634a1d5453f98a2214c38ea4c95e'
